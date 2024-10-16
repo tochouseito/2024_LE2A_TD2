500b0a9faf1917424957a51c0fef9a13c731e590
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2024-10-16 23:41:30 +0900

    UVスクロール完成

--- project/Game/Objects/GravityArrow/GravityArrow.h
+++ project/Game/Objects/GravityArrow/GravityArrow.h
@@ -7,15 +7,16 @@ public:
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize(Model* upModel, Model* downModel, ViewProjection* viewProjection, Vector3& pos);
-
-	void Update() const;
+	void Initialize(Model* upModel, Model* downModel, ViewProjection* viewProjection);
+	void Update();
 
 	void Draw();
 
 	void SetGravityDir(const bool up) {
 		isUp_ = up;
 	}
+
+	void SetPos(Vector3 vector3);
 
 	/*void SetPos(const Vector3 newPos) {
 		worldTransform_.translation_ = newPos;
@@ -31,16 +32,16 @@ public:
 
 
 private:
-	//WorldTransform worldTransform_;
-	std::vector<WorldTransform> worldTransforms_;
+	WorldTransform worldTransform_;
 	ViewProjection* viewProjection_ = nullptr;
 
 	Model* upModel_ = nullptr;
 	Model* downModel_ = nullptr;
 
+	float uvY = 0.0f;
+	float uvX = 0.0f;
+
 	bool isUp_ = false;
 
 	std::string upTextureHandle;
 };
-
-
--- project/Game/Scenes/GameScene.h
+++ project/Game/Scenes/GameScene.h
@@ -55,8 +55,8 @@
 #include "Objects/Goal/Goal.h"
 
 // DeltaTime
-#include "Needle.h"
 #include"Game/DeltaTime/DeltaTime.h"
+#include "Needle/Needle.h"
 
 class GameScene :
 	public BaseScene {
@@ -65,13 +65,15 @@ public:
 	~GameScene();
 
 	// Main
-	void Initialize()override;
-	void Finalize()override;
-	void Update()override;
-	void Draw()override;
+	void Initialize() override;
+	void Finalize() override;
+	void Update() override;
+	void Draw() override;
 
 	// シーンの切り替え依頼
-	void ChangeScene()override {}
+	void ChangeScene() override {
+	}
+
 	void SetSceneManager(SceneManager* sceneManager) override {
 		sceneManager_ = sceneManager;
 	}
@@ -110,7 +112,7 @@ private:
 
 
 	// デバッグカメラ
-	std::unique_ptr < DebugCamera> debugCamera_;
+	std::unique_ptr<DebugCamera> debugCamera_;
 	bool useDebugCamera_ = false;
 
 	std::unique_ptr<MainCamera> mainCamera_;
@@ -155,4 +157,3 @@ private:
 
 	std::unique_ptr<DeltaTime> deltaTime_ = nullptr;
 };
-
