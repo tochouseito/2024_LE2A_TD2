84b340b3cdf3f23f4e6f00e5753764bb1bc6b91b
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2024-10-21 19:54:03 +0900

    UNDONE

--- project/Game/Scenes/TitleScene.cpp
+++ project/Game/Scenes/TitleScene.cpp
@@ -1,17 +1,22 @@
 #include "TitleScene.h"
 #include"SceneManager.h"
 #include"imgui.h"
+#include "TextureManager.h"
 
-void TitleScene::Initialize()
-{
+void TitleScene::Initialize() {
+	viewProjection_.Initialize();
+	DirectXCommon::GetInstance()->SetViewProjection(&viewProjection_);
+
+
+	guiTextureHandle_ = TextureManager::GetInstance()->Load("./Resources/GUI/gui.png");
+
+	guiSprite_ = std::make_unique<Sprite>();
+	guiSprite_->Initialize({ 0.0f,0.0f,0.0f }, &viewProjection_, guiTextureHandle_);
 }
 
-void TitleScene::Finalize()
-{
-}
+void TitleScene::Finalize() {}
 
-void TitleScene::Update()
-{
+void TitleScene::Update() {
 #ifdef _DEBUG
 	ImGui::Begin("TitleScene");
 	if (ImGui::Button("GoStageSelect")) {
@@ -20,13 +25,38 @@ void TitleScene::Update()
 	}
 	ImGui::End();
 #endif // _DEBUG
+
+	//XINPUT_STATE controllerState;
+	//bool controllerConnected = Input::GetInstance()->GetJoystickState(0, controllerState);
+
+	//float stickX = 0.0f;
+	//if (controllerConnected) {s
+	//	stickX = static_cast<float>(controllerState.Gamepad.sThumbLX) / 32767.0f;  // 正規化したスティックのX入力（-1.0 ~ 1.0）
+	//}
+	//// 左右移動入力
+	//bool keyboardMoveLeft = Input::GetInstance()->PushKey(DIK_A);
+	//bool keyboardMoveRight = Input::GetInstance()->PushKey(DIK_D);
+	//bool controllerMoveLeft = stickX < -0.2f;
+	//bool controllerMoveRight = stickX > 0.2f;
+	//if (keyboardMoveLeft || keyboardMoveRight || controllerMoveLeft || controllerMoveRight) {
+	//	// 左右処理
+	//	Vector3 acceleration = {};
+	//	if (keyboardMoveRight || controllerMoveRight) {
+	//		// 左移動中の右入力
+	//	} else if (keyboardMoveLeft || controllerMoveLeft) {
+	//		// 右移動中の左入力
+	//	}
+	//}
+
+	guiSprite_->Update();
+
+	viewProjection_.UpdateMatrix();
 }
 
-void TitleScene::Draw()
-{
+void TitleScene::Draw() {
+	guiSprite_->Draw();
 }
 
-void TitleScene::ChangeScene()
-{
-	
+void TitleScene::ChangeScene() {
+
 }
--- project/Game/Scenes/TitleScene.h
+++ project/Game/Scenes/TitleScene.h
@@ -1,7 +1,10 @@
 #pragma once
+#include <memory>
+
 #include"BaseScene.h"
-class TitleScene:public BaseScene
-{
+#include "Sprite.h"
+
+class TitleScene :public BaseScene {
 public:
 	/// <summary>
 	/// 初期化
@@ -28,9 +31,15 @@ public:
 	/// </summary>
 	void ChangeScene()override;
 
-	void SetSceneManager(SceneManager* sceneManager) override{ sceneManager_ = sceneManager; }
+	void SetSceneManager(SceneManager* sceneManager) override { sceneManager_ = sceneManager; }
 
 private:
 	SceneManager* sceneManager_ = nullptr;
+
+	ViewProjection viewProjection_;
+
+	std::string guiTextureHandle_;
+
+	std::unique_ptr<Sprite> guiSprite_;
 };
 
